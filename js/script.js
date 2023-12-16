01805510f894a2361a09612720acc2db6437de4d
Claire <153375525+clairedotpng@users.noreply.github.com>
2023-12-16 22:28:45 +0100

    Fixed articles' div responsiveness on mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const splashes = [
     'American pizza\'s not real pizza.',
-    '‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌‌‌  Melons!',
-    '‌‌ ‌ ‌ ‌ ‌ ‌ ‌‌  Made in live!',
+    '‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌ ‌‌‌‌ Melons!',
+    '‌‌ ‌ ‌ ‌ ‌ ‌ ‌‌ Made in live!',
     'What a unique feature this is',
     '‌ ‌ ‌ ‌ ‌ ‌‌ ‌ Mankind is dead',
     '‌ ‌ ‌ ‌ ‌ ‌ ‌ ‌ Blood is fuel',
@@ -11,7 +11,6 @@ const splashes = [
     '‌ ‌ ‌ ‌ ‌ ‌ ‌ 39.102.67.150',
     'Really long message just to test the boundaries of this! You\'ll have to select me if you wanna read it all!'
 
-    
 ] 
 
 const randomSplash = Math.floor(Math.random() * splashes.length)
